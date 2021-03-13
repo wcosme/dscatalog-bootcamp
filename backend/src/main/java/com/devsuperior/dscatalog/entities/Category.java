05aad6b2783869bc542a3fbcfdd1cb7b9d96d943
Wallace Gonçalves <wallacecosme@gmail.com>
2021-03-13 19:08:39 -0300

    Criação das entities role e user

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -29,6 +32,9 @@ public class Category implements Serializable{
 	// Salva no banco o tempo no padrão UTC
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
@@ -96,5 +102,9 @@ public class Category implements Serializable{
 
 	public Instant getUpdatedAt() {
 		return updatedAt;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}	
 }
